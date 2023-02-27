3acbc5fca220acb6b4d9da1023dbab5850a509eb
Matt Rowlinson <mattnrowlinson@gmail.com>
2023-02-27 16:47:09 +0000

    frontend update

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://open-ai-codex-s67g.vercel.app/', {
+    const response = await fetch('https://codex-iau7.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
